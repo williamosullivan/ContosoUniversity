6b44f7cd22a62eb67efa7db77dec483046433df5
William O'Sullivan <williamosullivan@gmail.com>
2014-07-08 14:25:40 -0400

    Added Concurrency Errors
    Error handling for multiple users implemented

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -22,6 +22,9 @@ namespace ContosoUniversity.Models
         public DateTime StartDate { get; set; }
         public int? InstructorID { get; set; }
 
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+
         public virtual Instructor Administrator { get; set; }
         public virtual ICollection<Course> Courses { get; set; }
     }
