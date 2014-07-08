604923270a711ba562837ad7b706bca2883a2922
William O'Sullivan <williamosullivan@gmail.com>
2014-07-08 13:46:40 -0400

    Created Departments Section
    Created department section and deployed to Azure

--- ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -22,6 +22,8 @@ namespace ContosoUniversity.DAL
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
 
             modelBuilder.Entity<Course>().HasMany(c => c.Instructors).WithMany(i => i.Courses).Map(t => t.MapLeftKey("CourseID").MapRightKey("InstructorID").ToTable("CourseInstructor"));
+
+            modelBuilder.Entity<Department>().MapToStoredProcedures();
         }
     }
 }
